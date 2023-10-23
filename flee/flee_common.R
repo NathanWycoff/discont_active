ba14c6f6e98ccbbb0f66a7772e01fa9ce93434a8
Nathan Wycoff <nathanbrwycoff@gmail.com>
2023-10-23 19:01:57 -0400

    Removed old folder

--- flee/flee_common.R
+++
@@ -1,3 +0,0 @@
-
-dats <- c('ssudan' , 'mali' , 'syria' , 'ethiopia' , 'burundi', 'car')
-pretty_names <- list(car = 'Central African Republic', ssudan='South Sudan', mali='Mali', syria='Syria', ethiopia='Ethiopia', burundi = 'Burundi')
